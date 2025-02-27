44ba5903a4dceb286cd86671d63f4260b1d2184b
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-02-27 18:00:22 +0800

    Added cookieParser library

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./lib/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use("/auth", authRoutes);
 
