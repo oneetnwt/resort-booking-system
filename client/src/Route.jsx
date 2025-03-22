58b3e0e24e8e3aff1440311cc2003a19f4161121
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-03-23 00:44:17 +0800

    Added Not Found Route

--- client/src/Route.jsx
+++ client/src/Route.jsx
@@ -1,16 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import NotFound from "./page/NotFound";
 
 function route() {
   const route = createBrowserRouter([
     {
       path: "/",
       element: <App />,
-      errorElement: (
-        <h1 className="italic text-3xl text-[#ff0000] font-bold flex justify-center items-center h-screen">
-          404 Not Found
-        </h1>
-      ),
+      errorElement: <NotFound />,
     },
   ]);
   return <RouterProvider router={route} />;
